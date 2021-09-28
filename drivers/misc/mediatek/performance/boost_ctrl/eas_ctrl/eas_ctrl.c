8aacbb86d6921fcd8460ba0437aab200fe01b7ea
ZyCromerZ <neetroid97@gmail.com>
2021-09-28 13:21:52 +0700

    kernel: sched: blu_schedutil: update symbol for blu_schedutil
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- drivers/misc/mediatek/performance/boost_ctrl/eas_ctrl/eas_ctrl.c
+++ drivers/misc/mediatek/performance/boost_ctrl/eas_ctrl/eas_ctrl.c
@@ -189,7 +189,7 @@ int update_schedplus_down_throttle_ns(int kicker, int nsec)
 	cur_schedplus_down_throttle_ns = final_down_thres < 0 ?
 		-1 : final_down_thres;
 
-#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL || defined CONFIG_CPU_FREQ_GOV_BLU_SCHEDUTIL
+#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL
 	if (debug_schedplus_down_throttle_nsec == -1) {
 #if defined(CONFIG_CPUFREQ_HAVE_GOVERNOR_PER_POLICY)
 		if (cur_schedplus_down_throttle_ns >= 0)
@@ -206,6 +206,28 @@ int update_schedplus_down_throttle_ns(int kicker, int nsec)
 				cur_schedplus_down_throttle_ns / 1000);
 		else
 			schedutil_set_down_rate_limit_us(0,
+				default_schedplus_down_throttle_ns / 1000);
+#endif
+	}
+#endif
+
+#if defined(CONFIG_CPU_FREQ_GOV_BLU_SCHEDUTIL)
+	if (debug_schedplus_down_throttle_nsec == -1) {
+#if defined(CONFIG_CPUFREQ_HAVE_GOVERNOR_PER_POLICY)
+		if (cur_schedplus_down_throttle_ns >= 0)
+			for (i = 0; i < cluster_num; i++)
+				blu_schedutil_set_down_rate_limit_us(cpu_id[i],
+					cur_schedplus_down_throttle_ns / 1000);
+		else
+			for (i = 0; i < cluster_num; i++)
+				blu_schedutil_set_down_rate_limit_us(cpu_id[i],
+					default_schedplus_down_throttle_ns / 1000);
+#else
+		if (cur_schedplus_down_throttle_ns >= 0)
+			blu_schedutil_set_down_rate_limit_us(0,
+				cur_schedplus_down_throttle_ns / 1000);
+		else
+			blu_schedutil_set_down_rate_limit_us(0,
 				default_schedplus_down_throttle_ns / 1000);
 #endif
 	}
@@ -259,7 +281,7 @@ int update_schedplus_up_throttle_ns(int kicker, int nsec)
 	cur_schedplus_up_throttle_ns = final_up_thres < 0 ?
 		-1 : final_up_thres;
 
-#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL || defined CONFIG_CPU_FREQ_GOV_BLU_SCHEDUTIL
+#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL
 	if (debug_schedplus_up_throttle_nsec == -1) {
 #if defined(CONFIG_CPUFREQ_HAVE_GOVERNOR_PER_POLICY)
 		if (cur_schedplus_up_throttle_ns >= 0)
@@ -276,6 +298,29 @@ int update_schedplus_up_throttle_ns(int kicker, int nsec)
 				cur_schedplus_up_throttle_ns / 1000);
 		else
 			schedutil_set_up_rate_limit_us(0,
+				default_schedplus_up_throttle_ns / 1000);
+#endif
+	}
+#endif
+
+
+#if defined(CONFIG_CPU_FREQ_GOV_BLU_SCHEDUTIL)
+	if (debug_schedplus_up_throttle_nsec == -1) {
+#if defined(CONFIG_CPUFREQ_HAVE_GOVERNOR_PER_POLICY)
+		if (cur_schedplus_up_throttle_ns >= 0)
+			for (i = 0; i < cluster_num; i++)
+				blu_schedutil_set_up_rate_limit_us(cpu_id[i],
+					cur_schedplus_up_throttle_ns / 1000);
+		else
+			for (i = 0; i < cluster_num; i++)
+				blu_schedutil_set_up_rate_limit_us(cpu_id[i],
+					default_schedplus_up_throttle_ns / 1000);
+#else
+		if (cur_schedplus_up_throttle_ns >= 0)
+			blu_schedutil_set_up_rate_limit_us(0,
+				cur_schedplus_up_throttle_ns / 1000);
+		else
+			blu_schedutil_set_up_rate_limit_us(0,
 				default_schedplus_up_throttle_ns / 1000);
 #endif
 	}
@@ -1181,12 +1226,20 @@ static ssize_t perfmgr_debug_schedplus_down_throttle_proc_write(
 	mutex_lock(&boost_eas);
 	debug_schedplus_down_throttle_nsec = data;
 
-#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL || defined CONFIG_CPU_FREQ_GOV_BLU_SCHEDUTIL
+#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL
 	if (data == -1)
 		schedutil_set_down_rate_limit_us(0,
 			cur_schedplus_down_throttle_ns);
 	else if (data >= 0)
 		schedutil_set_down_rate_limit_us(0, data / 1000);
+#endif
+
+#if defined(CONFIG_CPU_FREQ_GOV_BLU_SCHEDUTIL)
+	if (data == -1)
+		blu_schedutil_set_down_rate_limit_us(0,
+			cur_schedplus_down_throttle_ns);
+	else if (data >= 0)
+		blu_schedutil_set_down_rate_limit_us(0, data / 1000);
 #endif
 
 	mutex_unlock(&boost_eas);
@@ -1240,12 +1293,20 @@ static ssize_t perfmgr_debug_schedplus_up_throttle_proc_write(
 	mutex_lock(&boost_eas);
 	debug_schedplus_up_throttle_nsec = data;
 
-#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL || defined CONFIG_CPU_FREQ_GOV_BLU_SCHEDUTIL
+#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL
 	if (data == -1)
 		schedutil_set_up_rate_limit_us(0,
 			cur_schedplus_up_throttle_ns);
 	else if (data >= 0)
 		schedutil_set_up_rate_limit_us(0, data / 1000);
+#endif
+
+#if defined(CONFIG_CPU_FREQ_GOV_BLU_SCHEDUTIL)
+	if (data == -1)
+		blu_schedutil_set_up_rate_limit_us(0,
+			cur_schedplus_up_throttle_ns);
+	else if (data >= 0)
+		blu_schedutil_set_up_rate_limit_us(0, data / 1000);
 #endif
 
 	mutex_unlock(&boost_eas);
